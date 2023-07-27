2eecdb9dbd63be1c59f464b3a51393c3eb3d2e46
AntonioRubiaSol <Antonio.Rubia@solera.com>
2023-07-27 17:00:20 +0200

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/antoniorl01/websockets/internal/handlers"
+	"github.com/bmizerany/pat"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	return mux
+}
